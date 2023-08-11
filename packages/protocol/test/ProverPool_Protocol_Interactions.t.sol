ec02b42e89877fd494776373a9630aa0929e554a
d1onys1us <13951458+d1onys1us@users.noreply.github.com>
2023-08-10 21:16:02 -0400

    cleanup todos

--- packages/protocol/test/ProverPool_Protocol_Interactions.t.sol
+++ packages/protocol/test/ProverPool_Protocol_Interactions.t.sol
@@ -264,8 +264,8 @@ contract TaikoL1ProverPool is TaikoL1TestBase {
         }
         printVariables("");
     }
-    // TODO(docs)
 
+    // TODO(docs): spelling errors `asigned`
     function test_asigned_prover_distribution_if_only_have_1_prover()
         external
     {
